af781e003a6af8716444ba82f247df2e87ed3d05
milosnowcat <ramiro.alvarez@rahcode.com>
2023-08-31 10:59:50 -0600

    TODO

---
+++ main.py
@@ -0,0 +1,4 @@
+# TODO hacer un programa en terminal para guardar claves,
+# usara una clave maestra para encriptar las claves,
+# copiara la clave al portapaeles para no mostrarla en la terminal,
+# podra generar claves aleatorias
